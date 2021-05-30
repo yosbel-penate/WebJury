56c1102edc137b4701e131eb429f343825d8fb05
yosbel <yosbel.penate@gmail.com>
2021-05-30 23:12:13 +0300

    +++

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=e:\Work\WebJury
+set VIRTUAL_ENV=E:\Work\WebJury
 
 if not defined PROMPT set PROMPT=$P$G
 
